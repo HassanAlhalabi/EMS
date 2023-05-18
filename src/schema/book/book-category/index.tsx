30fff1048c7967e73d6003f36f4f6f0102b64400
Hassan Alhalabi <hassanalhalabi1997@outlook.com>
2023-05-18 12:04:05 +0300

    Fix Book Image Schema

--- src/schema/book/book-category/index.tsx
+++ src/schema/book/book-category/index.tsx
@@ -3,6 +3,6 @@ import { object, string } from "yup";
 export const bookCategoryValidation = object({
     nameAr: string().required('Arabic Name Is Required'),
     nameEn: string().required('English Name Is Required'),
-    image: string(),
+    image: object().nullable(),
     superCategoryId: string().nullable()
 })
