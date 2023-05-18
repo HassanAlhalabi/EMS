2db4dbdf03ce7e65a9a99085583391a8721cc024
Hassan Alhalabi <hassanalhalabi1997@outlook.com>
2023-05-18 12:04:35 +0300

    Fix Speciality Schema

--- src/schema/subject/index.ts
+++ src/schema/subject/index.ts
@@ -10,7 +10,7 @@ export const subjectValidation = object({
     specialtySubjects: array().of(object({
         specialtyId: string().required('Speciality Is Required'),
         superSubjectId: string().nullable()
-    })).required('Specialities Are Required')
+    })).required('Specialities Are Required').min(1,'Specialities Are Required')
 })
 
 export const subjectTypeValidation = object({
