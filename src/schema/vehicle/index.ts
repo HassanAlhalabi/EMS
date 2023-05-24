703b23624a0676a6660f5cceb7967c4e61158648
Hassan Alhalabi <hassanalhalabi1997@outlook.com>
2023-05-24 15:36:10 +0300

    Update Vehicle Schema

--- src/schema/vehicle/index.ts
+++ src/schema/vehicle/index.ts
@@ -3,6 +3,6 @@ import { number, object, string } from "yup";
 export const vehicleValidation = object({
     vehicleBrand: string().required('Vehicle Brand Is Required'),
     vehiclePlate: string().required('Vehicle Plate Number Is Required'),
-    vehicleCapacity: number().required('Vehicle Capacity'),
-    endClassChairCount: number().required('End Chair Count Is Required')
+    vehicleCapacity: number().required('Vehicle Capacity').min(20),
+    endClassChairCount: number().required('End Chair Count Is Required').min(2)
 })
