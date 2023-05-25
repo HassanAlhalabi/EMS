46de5e1e326be274f13d1dfea122e54755c5ad82
Hassan Alhalabi <hassanalhalabi1997@outlook.com>
2023-05-25 15:12:58 +0300

    Add Bus Stop Schema

---
+++ src/schema/busstop/index.ts
@@ -0,0 +1,8 @@
+import { number, object, string } from "yup";
+
+export const busstopValidation = object({
+    nameAr: string().required('Arabic Name Is Required'),
+    nameEn: string().required('English Name Is Required'),
+    cityId: number().required('City Is Required').min(0,'City Is Required'),
+    stateId: number().required('State Is Required').min(0,'State Is Required')
+})
