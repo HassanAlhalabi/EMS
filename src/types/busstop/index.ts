c2cafaa37d3656fade862b8fc8b569bb2d0efcb9
Hassan Alhalabi <hassanalhalabi1997@outlook.com>
2023-05-25 15:12:33 +0300

    Add State,City, BusStop Types

---
+++ src/types/busstop/index.ts
@@ -0,0 +1,18 @@
+
+export interface NewBusStop {
+    nameAr: string;
+    nameEn: string;
+    cityId: number;
+    stateId: number;
+}
+
+export interface BusStop {
+    id: string;
+    nameAr: string;
+    nameEn: string;
+    cityName: string;
+    cityId: number;
+    stateId: number;
+    stateName: string;
+    
+}
---
+++ src/types/state/index.ts
@@ -0,0 +1,9 @@
+
+
+export interface State {
+    stateId: number,
+    stateName: string,
+    iso2: string,
+    latitude: number,
+    longitude: number
+}
