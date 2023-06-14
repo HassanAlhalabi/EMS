e1e2c29b2649bcee43fd4736e3e85600d278c702
Hassan Alhalabi <hassanalhalabi1997@outlook.com>
2023-06-14 15:41:48 +0300

    Add localhost Proxy URL

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'https://alimakhlouf-002-site4.btempurl.com'
+    }
+  }
 })
