416b361f14ff337b207d58be45837b1ed3c812ce
Hassan Alhalabi <hassanalhalabi1997@gmail.com>
2023-07-09 10:17:31 +0300

    Setup Test Enviroment

--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,12 @@ export default defineConfig({
         secure: false
       }
     }
-  }
+  },
+  test: {
+    environment: "jsdom",
+    setupFiles: "./setupTests.ts",
+    // speed up since tests don't rely on css
+    // https://github.com/vitest-dev/vitest/blob/main/examples/react-testing-lib/vite.config.ts#L14-L16
+		css: false,
+  },
 })
